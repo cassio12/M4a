1e84e466b104ab5d574f3608812dcdf612d8a2f1
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2018-09-28 15:59:47 -0300

    update

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, FlatList, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, FlatList, TextInput } from 'react-native';
 import {TabNavigator, StackNavigator} from 'react-navigation'
 
 import OngScreen from './source/screens/ongScreen';
 
 class App extends Component {
 
-  static navigationOptions = ({navigation}) => ({
-    title:'bem vindo'
-  })
+  // static navigationOptions = ({navigation}) => ({
+  //   title:'bem vindo'
+  // })
 
   render() {
     
     return (
-         <Button title="ONG" onPress={() => this.props.navigation.navigate('Ong') } />
+      <View style={styles.container}>
+        <View style={styles.containerLogin}>
+          <Image style={styles.logo} source={require('./source/assets/m4a.png')}/>
+          <View style={styles.boxInput}>
+            <Text>Log in</Text>
+            <TextInput style={styles.input} title='Email'/>
+            <TextInput style={styles.input} title='Password'/>
+            {/* <Button title="Log in" onPress={() => this.props.navigation.navigate('Ong') } /> */}
+          </View>
+        </View>
+      </View>
     );
   }
 }
@@ -23,11 +33,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,  
     width:'100%',
-    backgroundColor: '#fff',
+    backgroundColor: '#38A800',
     alignItems: 'center',
     paddingTop: 20,
     flexDirection: 'column',
-  }
+  },
+  containerLogin:{
+    width:'95%',
+    height:'90%',
+    borderRadius: 30,
+    backgroundColor: '#fff',
+  },
+  logo:{
+    width: '30%',
+    height: 100,
+    alignItems:'center',
+  },
+  input:{
+    width:'100%',
+    borderRadius:30,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderStyle: 'dashed',
+  },
+  boxInput:{
+    width: '90%',
+    height:100,
+    alignSelf:'center',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    
+  },
+
 });
 
 
